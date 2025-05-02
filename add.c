801b2059b023d8f8404aa9b5a5c51c27d96011d1
Shreya Shekhara <158731055+ShreyaUdupi05@users.noreply.github.com>
2025-05-02 14:39:43 +0530

    Update  array add.c

--- add.c
+++ add.c
@@ -3,9 +3,18 @@ int main()
 
 {
     int x,y;
+    int arr[20];
     printf("enter the number x & y ");
     scanf("%d%d",&x,&y);
     int sum =x+y;
     printf("%d",sum);
+    printf("Enter the array elements: ");
+    for(int i =0;i<5;i++)
+        {
+    scanf("%d",arr[i]);
+        }
+    printf("%d",arr);
+
+    
     return 0;
 }
