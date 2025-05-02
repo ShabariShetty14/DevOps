a03a659ae3d5a4582129dfa457b376294cb07bcf
unknown <yashrajnpai7@gmail.com>
2025-05-02 11:12:42 +0530

    first commit

---
+++ prgm1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+        char UserName[] = "admin@123";
+        scanf("%s", &UserName);
+        printf("enter your password: \n");
+        char PassWord[] = "123456789";
+        scanf("%s", &PassWord); 
+        if (UserName == "admin@123") 
+        {
+        if (PassWord == "123456789"){
+        printf("Login Successful\n");
+        }
+        }else
+        {
+        printf("Login credentials are incorrect.\n");
+        }}
+   
